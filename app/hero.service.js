0a4f6329a63c2ebc734ce30f11bcac2f7ae0b512
Dmytro Lukianov <lukianov.d.o@gmail.com>
2016-10-01 19:47:27 +0300

    Create Hero Editor

---
+++ app/hero.service.js
@@ -0,0 +1,20 @@
+"use strict";
+var mock_heroes_1 = require('./mock-heroes');
+;
+var HeroService = (function () {
+    function HeroService() {
+    }
+    HeroService.prototype.getHeroes = function () {
+        return Promise.resolve(mock_heroes_1.HEROES);
+    };
+    HeroService.prototype.getHeroesSlowly = function () {
+        var _this = this;
+        return new Promise(function (resolve) {
+            return setTimeout(resolve, 2000);
+        }) // delay 2 seconds
+            .then(function () { return _this.getHeroes(); });
+    };
+    return HeroService;
+}());
+exports.HeroService = HeroService;
+//# sourceMappingURL=hero.service.js.map
